a22892e239b9739e0e97ad19e27244580e2392ad
Losevsa <Losevsa@inbox.ru>
2021-12-11 13:29:16 +0300

    finish(1)

--- ConsoleApplication4/ConsoleApplication4.cpp
+++ ConsoleApplication4/ConsoleApplication4.cpp
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 
 
+
 enum switches {
 	POWER = 1,
 	MAIN_OUTLETS = 2,
@@ -16,22 +17,6 @@ enum switches {
 };
 
 
-
-//enum switches {
-//	OUTLETS = 1,
-//	BOILER = 2,
-//	OUTLETS_2 = 4,
-//	LIGHT = 8,
-//	LIGHT_2 = 16,
-//};
-//int state = 0;
-//state |= OUTLETS;
-//state &= ~LIGHT;
-//
-//if (state & LIGHT) {
-//	std::cout << " Light on";
-//}
-
 int main()
 {	
 	int status = 0;
@@ -42,17 +27,20 @@ int main()
 	int hour = 0;
 	int day = 1;
 	std::cout << "Smart house..." << std::endl;
-
-
 	
 	while (true) {
+		hour++;
+		if (hour > 23) {
+			hour = 0;
+			day++;
+		}
 		std::cout << std::endl;
 		std::cout << "Enter sensor readings(temp outside '15', temp inside '25', movement 'No', lights 'No')" << std::endl;
 		std::getline(std::cin, temp);
 		system("cls");
 		buffer << temp;
 		buffer >> tempOutside >> tempInside >> movement >> lights;
-		
+
 
 		std::cout << "Day: " << day << ". Time: " << hour << ":00" << std::endl;
 		std::cout << "Current temperature outside: " << tempOutside << std::endl;
@@ -60,11 +48,9 @@ int main()
 		std::cout << "There is movement otside: " << movement << std::endl;
 		std::cout << "Light in the house: " << lights << std::endl;
 		std::cout << "------------------------------------------" << std::endl;
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////
-		// вкл/выкл датчики
-
-		// проверка на температуру вне дома и включении подогрева труб
+		
+		// проверяем и выводит статусы датчиков
+	// проверка на температуру вне дома и включении подогрева труб
 		if (tempOutside < 0) {
 			if (!(status & TEMP_WATER_PIPES)) {
 				status |= TEMP_WATER_PIPES;
@@ -76,7 +62,7 @@ int main()
 				status &= ~TEMP_WATER_PIPES;
 				std::cout << "Pipes - off" << std::endl;
 			}
-			
+
 		}
 
 		// с 16:00 до 5:00 - свет вкл, если есть движение
@@ -85,7 +71,7 @@ int main()
 				status |= LIGHT_OUTSIDE;
 				std::cout << "LIGHT_OUTSIDE - on" << std::endl;
 			}
-			
+
 		}
 		else if (status & LIGHT_OUTSIDE) {
 			std::cout << "LIGHT_OUTSIDE - off" << std::endl;
@@ -98,14 +84,14 @@ int main()
 				status |= TEMP_INSIDE;
 				std::cout << "TEMP_INSIDE - on" << std::endl;
 			}
-			
+
 		}
 		else if (tempInside > 24) {
 			if (status & TEMP_INSIDE) {
 				status &= ~TEMP_INSIDE;
 				std::cout << "TEMP_INSIDE - off" << std::endl;
 			}
-			
+
 		}
 
 		// enable air cond
@@ -114,29 +100,30 @@ int main()
 				status |= AIR_COND;
 				std::cout << "AIR_COND - on" << std::endl;
 			}
-			
+
 		}
 		else if (tempInside < 26) {
 			if (status & AIR_COND) {
 				status &= ~AIR_COND;
 				std::cout << "AIR_COND - off" << std::endl;
 			}
-			
+
 		}
 
 		//5000 - 2700 
-		
+
 		if (hour == 16) k = 4540;
 		else if (hour == 17) k = 4080;
 		else if (hour == 18) k = 3620;
 		else if (hour == 19) k = 3160;
 		else if (hour == 20) k = 2700;
 		else if (hour == 0) k = 5000;
-		if (lights == "on" && (hour >= 16 && hour <= 20)) {
+		if (lights == "on" && ((hour >= 16 && hour <= 20) || hour == 0)) {
 			if (!(status & LIGHT_INSIDE_HOME)) {
 				status |= LIGHT_INSIDE_HOME;
-				std::cout << "Light in home - on, " << k << "K" << std::endl;
+				
 			}
+			std::cout << "Light in home - on, " << k << "K" << std::endl;
 		}
 		else if (lights == "on") {
 			if (!(status & LIGHT_INSIDE_HOME)) {
